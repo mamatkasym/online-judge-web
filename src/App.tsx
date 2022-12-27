92d132ddf086378c0c6c01ae9a53e4eaa57b7d24
mamatkasym <mm.kalandar@gmail.com>
2022-12-28 05:20:12 +0600

    rename the page

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Home from "./pages/Home.tsx"
 // @ts-ignore
 import Problems from "./pages/Problems.tsx"
 // @ts-ignore
-import ProblemStatement from "./pages/ProblemStatement.tsx"
+import Problem from "./pages/Problem.tsx"
 // @ts-ignore
 import Login from "./components/Login.tsx"
 // @ts-ignore
@@ -20,7 +20,7 @@ function App() {
           <Routes>
               <Route path="/" element={<Home/>}>
               <Route path="problems" element={<Problems/>}/>
-              <Route path="problems/:problemId" element={<ProblemStatement/>}/>
+              <Route path="problems/:problemId" element={<Problem/>}/>
               <Route path="contests" element={<div>Contests page is under development...</div>}/>
               <Route path="login" element={<Login/>}/>
               <Route path="register" element={<Register/>}/>
